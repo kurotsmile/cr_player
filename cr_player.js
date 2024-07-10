7b683dbcb3603c29ddb67d4559408e971abbd8b0
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-10 17:36:07 +0700

    Add playlist for cr_player

--- cr_player.js
+++ cr_player.js
@@ -9,27 +9,65 @@ class CR_Player {
   name_song="Carrot Player Music";
   name_singer="Music Player";
 
+  list_song=[];
+  
   onCreate() {
     this.audio_player = new Audio();
     $('head').append('<link rel="stylesheet" type="text/css" href="cr_player/theme_' + this.theme + '.css">');
   }
 
   play(url_mp3) {
+    this.list_song=[];
+    var obj_data={"mp3":url_mp3,"name":"Song "+this.list_song.length,"artist":"Carrot Player Music"};
+    this.list_song.push(obj_data);
     this.set_mp3(url_mp3).audio_player.play();
     this.uiPlayer();
   }
 
   play(url_mp3,name_song){
+    this.list_song=[];
+    var obj_data={"mp3":url_mp3,"name":name_song,"artist":"Carrot Player Music"};
+    this.list_song.push(obj_data);
     this.name_song=name_song;
     this.set_mp3(url_mp3).audio_player.play();
     this.uiPlayer();
   }
 
   play(url_mp3,name_song,name_singer){
+    this.list_song=[];
+    var obj_data={"mp3":url_mp3,"name":name_song,"artist":name_singer};
+    this.list_song.push(obj_data);
     this.name_song=name_song;
     this.name_singer=name_singer;
     this.set_mp3(url_mp3).audio_player.play();
     this.uiPlayer();
+  }
+
+  add_song(url_mp3){
+    var obj_data={"mp3":url_mp3,"name":"Song "+this.list_song.length,"artist":"Carrot Player Music"};
+    this.list_song.push(obj_data);
+    this.check_add_song_to_playlist();
+  }
+
+  add_song(url_mp3,name,artist){
+    var obj_data={"mp3":url_mp3,"name":name,"artist":artist};
+    this.list_song.push(obj_data);
+    this.check_add_song_to_playlist();
+  }
+
+  check_add_song_to_playlist(){
+    swal.fire({
+      icon:'success',
+      title:'Add songs to the list',
+      Text:'Add song to playlist successfully!'
+    })
+
+    if(this.list_song.length==1){
+      this.set_mp3(url_mp3).audio_player.play();
+      this.uiPlayer();
+    }else{
+      this.updateMetaInfo();
+    }
   }
 
   set_mp3(url_mp3) {
@@ -46,19 +84,29 @@ class CR_Player {
       html+='<div id="cr_singer" style="color:'+this.color_hightlight+'">'+this.name_singer+'</div>';
       html+='</div>';
       html+='<button onclick="cr_player.play();" class="btn btn-sm btn-dark ml-2"><i class="far fa-play-circle"></i></button>';
-      html+='<button onclick="cr_player.stop();" class="btn btn-sm btn-dark ml-1"><i class="fas fa-step-backward"></i></button>';
-      html+='<button onclick="cr_player.stop();" class="btn btn-sm btn-dark ml-1"><i class="fas fa-step-forward"></i></button>';
+      html+='<button onclick="cr_player.stop();" class="btn btn-sm btn-dark ml-1" id="cr_btn_next"><i class="fas fa-step-backward"></i></button>';
+      html+='<button onclick="cr_player.stop();" class="btn btn-sm btn-dark ml-1" id="cr_btn_prev"><i class="fas fa-step-forward"></i></button>';
       html+='<button onclick="cr_player.stop();" class="btn btn-sm btn-dark ml-1"><i class="far fa-stop-circle"></i></button>';
-      html+='<button onclick="cr_player.hide();" class="btn btn-sm btn-dark ml-1 btn-setting"><i class="fas fa-tools"></i></button>';
+      html+='<button onclick="cr_player.show_setting();" class="btn btn-sm btn-dark ml-1 btn-setting"><i class="fas fa-tools"></i></button>';
       html+='</div>';
       this.emp_ui_player=$(html);
       $("body").append(this.emp_ui_player);
-      $(this.emp_ui_player).find("#cr_name").html(this.name_song);
-      $(this.emp_ui_player).find("#cr_singer").html(this.name_singer);
+      this.updateMetaInfo();
     }else{
       $(this.emp_ui_player).fadeIn(500);
-      $(this.emp_ui_player).find("#cr_name").html(this.name_song);
-      $(this.emp_ui_player).find("#cr_singer").html(this.name_singer);
+      this.updateMetaInfo();
+    }
+  }
+
+  updateMetaInfo(){
+    $(this.emp_ui_player).find("#cr_name").html(this.name_song);
+    $(this.emp_ui_player).find("#cr_singer").html(this.name_singer);
+    if(this.list_song.length>1){
+      $("#cr_btn_next").show();
+      $("#cr_btn_prev").show();
+    }else{
+      $("#cr_btn_next").hide();
+      $("#cr_btn_prev").hide();
     }
   }
 
@@ -114,5 +162,11 @@ class CR_Player {
     }
   }
 
+  show_setting(){
+    swal.fire({
+      title:"Setting",
+      Text:"Conetent"
+    });
+  }
 }
 var cr_player = new CR_Player();
